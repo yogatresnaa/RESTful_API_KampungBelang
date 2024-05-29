89afc7e928a71074e6b858e04408ec914b581a35
yogatresnaa <yogatresnaa@gmail.com>
2024-05-29 15:45:32 +0700

    delet origin spesifi

--- src/server.js
+++ src/server.js
@@ -37,7 +37,7 @@ const init = async () => {
     host: process.env.HOST,
     routes: {
       cors: {
-        origin: ['https://resto-kampungbelang.netlify.app'],
+        origin: ['*'],
       },
     },
   });
